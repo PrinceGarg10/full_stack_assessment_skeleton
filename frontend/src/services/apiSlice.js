4360aaae49c038e8dac8c667929599a75e9eba6b
MERN_DEV <dev@apnaschoolapp.com>
2024-09-01 15:48:26 +0530

    file name fixes

--- frontend/src/services/apiSlice.js
+++ frontend/src/services/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { API_URL, BASE_URL } from './api.url';
+import { API_URL, BASE_URL } from './apiUrl';
 
 export const apiSlice = createApi({
     reducerPath: 'api',
